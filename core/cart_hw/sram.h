cd27472755e981f86953eceb73fc43c489758c7d
Vladimir Kononovich <meffi@lab313.ru>
2018-12-13 05:49:36 +0300

    Added hex editor (TODO: fix cram editing).

--- core/cart_hw/sram.h
+++ core/cart_hw/sram.h
@@ -2,7 +2,7 @@
  *  Genesis Plus
  *  Backup RAM support
  *
- *  Copyright (C) 2007-2026  Eke-Eke (Genesis Plus GX)
+ *  Copyright (C) 2007-2016  Eke-Eke (Genesis Plus GX)
  *
  *  Redistribution and use of this code or any derivative works are permitted
  *  provided that the following conditions are met:
@@ -39,28 +39,23 @@
 #ifndef _SRAM_H_
 #define _SRAM_H_
 
-typedef enum
-{
-  DEFAULT_SRAM = 0,
-  EEPROM_I2C,
-  EEPROM_SPI,
-  EEPROM_MICROWIRE,
-  FLASH_MEMORY
-} E_SRAM_TYPE;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct
 {
   uint8 detected;
   uint8 on;
-  uint8 type;
+  uint8 custom;
   uint32 start;
   uint32 end;
   uint32 crc;
-  uint8 sram[0x10000];
+  uint8 *sram;
 } T_SRAM;
 
 /* Function prototypes */
-extern void sram_init(void);
+extern void sram_init();
 extern unsigned int sram_read_byte(unsigned int address);
 extern unsigned int sram_read_word(unsigned int address);
 extern void sram_write_byte(unsigned int address, unsigned int data);
@@ -68,5 +63,9 @@ extern void sram_write_word(unsigned int address, unsigned int data);
 
 /* global variables */
 extern T_SRAM sram;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
